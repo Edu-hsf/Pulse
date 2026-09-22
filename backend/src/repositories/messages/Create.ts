d5240d1d2514cc02edfe555e47c07e23b3a04b45
Eduardo <eduardo.h.fernandes.oficial@gmail.com>
2026-09-22 11:21:11 -0300

    fix: create messages agora permite criar um attachment junto

--- backend/src/repositories/messages/Create.ts
+++ backend/src/repositories/messages/Create.ts
@@ -1,10 +1,30 @@
 import pool from "../../config/database";
 import { CreateMessageDTO } from "../../types/message";
 
-export function Create (data: CreateMessageDTO) {
-    return pool.query(
-        `INSERT INTO MESSAGES (USER_ID, CONVERSATION_ID, CONTENT)
-        VALUES ($1, $2, $3)`,
-        [data.participantId, data.conversationId, data.content]
-    );
+export async function Create (data: CreateMessageDTO) {
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+
+        const messageResult = await client.query(
+        `INSERT INTO MESSAGES (PARTICIPANT_ID, CONVERSATION_ID, CONTENT)
+        VALUES ($1, $2, $3)
+        RETURNING id`,
+        [data.participantId, data.conversationId, data.content ?? null]);
+
+        const messageId = messageResult.rows[0].id;
+
+        if (data.attachment) {  
+            await client.query(`INSERT INTO MESSAGE_ATTACHMENTS (MESSAGE_ID, TYPE, URL) VALUES ($1, $2, $3)`, [messageId, data.attachment.type, data.attachment.url]);
+        }
+        
+        await client.query('COMMIT');
+
+    } catch (error) {
+        await client.query('ROLLBACK');
+        throw error;
+    } finally {
+        client.release();
+    }
 }
